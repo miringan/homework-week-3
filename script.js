fe408ba355211664478cf3570dafc82ced4f3975
Michael Iringan <miringan@uw.edu>
2021-11-15 23:47:12 -0800

    Added definition for the generatePassword() function, declared variables within generatePassword(). Added notes in the form of pseudo code.

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Prompt user for criteria
+// User selects criteria
+// Prompt user for length of password - needs to be between 8 and 128 characters
+// User enters in a password length between 8 and 128 characters
+// Prompt user for character types to include in the password
+// User confirms whether or not to include lowercase, uppercase, numeric, and/or special characters
+// Each answer should be validated and a random selection of the criteria should be selected
+// After all the prompts are answered, a password is generated.
+// The password is then displayed in an alert or written to the page.
+
+function generatePassword () {
+    //define variables
+    passwordLength;
+    passwordSpecialChar;
+    passwordHasUpCase;
+    passwordHasLowCase;
+
+    // if (passwordLength > 7 && passwordLength < 129) {
+    //     passwordLength = prompt("Set a password length between 8 and 128 characers.");
+    // }
+}
