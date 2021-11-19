ee7ece6f7bd3ca39da3c81df9b0a946b34ded482
Michael Iringan <miringan@uw.edu>
2021-11-18 17:01:48 -0800

    Overhauled script.js file. Added content to the generatePassword() function. Code reflects required behavior from the homework requirements. The notes will be updated on the next push.

--- script.js
+++ script.js
@@ -23,14 +23,113 @@ generateBtn.addEventListener("click", writePassword);
 // After all the prompts are answered, a password is generated.
 // The password is then displayed in an alert or written to the page.
 
-function generatePassword () {
-    //define variables
-    passwordLength;
-    passwordSpecialChar;
-    passwordHasUpCase;
-    passwordHasLowCase;
+// Create and populate array of lower case letters.
+const lowerCaseAlpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-    // if (passwordLength > 7 && passwordLength < 129) {
-    //     passwordLength = prompt("Set a password length between 8 and 128 characers.");
-    // }
+// Create array of upper case letters.
+const upperCaseAlpha = [];
+
+// Populate upperCaseAlpha.
+for (var i = 0; i < lowerCaseAlpha.length; i++) {
+  upperCaseAlpha[i] = lowerCaseAlpha[i].toUpperCase();
+}
+
+// Create and populate array of numbers.
+const num = [0,1,2,3,4,5,6,7,8,9];
+
+// Create and populate array of special characters. sans backslash.
+const specChar = [" ","!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
+
+
+function generatePassword () {
+  // Prompt for password length.
+  var passwordLength;
+  
+  function askPassLength() {
+    passwordLength = prompt("Set a password length between 8 and 128 characters.");
+
+    if (passwordLength < 8 || passwordLength > 128) {
+      alert("Password length is not within the acceptable length range.");
+      askPassLength();
+    } 
+  } 
+
+  askPassLength();
+
+  // if (passwordLength < 8 || passwordLength > 128) {
+  //   alert("Password length is not within the acceptable length range.");
+  //   passwordLength = prompt("Set a password length between 8 and 128 characers.");
+  // } 
+
+
+
+  // var passwordSpecialChar = confirm("Would you like to include special characers?");
+  // var passwordHasUpCase = confirm("Would you like to include upper case letters?");
+  // var passwordHasLowCase = confirm("Would you like to include lower case letters?");
+  // var passwordHasNums = confirm("Would you like to include numbers?");
+
+  // if (passwordSpecialChar === false && passwordHasUpCase === false && passwordHasLowCase === false && passwordHasNums === false) {
+  //   alert("You must choose to include one of the aforementioned password criteria to include in your password.");
+  //   passwordSpecialChar = confirm("Would you like to include special characers?");
+  //   passwordHasUpCase = confirm("Would you like to include upper case letters?");
+  //   passwordHasLowCase = confirm("Would you like to include lower case letters?");
+  //   passwordHasNums = confirm("Would you like to include numbers?");
+  // }
+
+  let generatedPass = [];
+
+  let password = "hellow world.";
+
+  function askPassRequirements () {
+    var passwordSpecialChar = confirm("Would you like to include special characers?");
+    var passwordHasUpCase = confirm("Would you like to include upper case letters?");
+    var passwordHasLowCase = confirm("Would you like to include lower case letters?");
+    var passwordHasNums = confirm("Would you like to include numbers?");
+
+    if (passwordSpecialChar === false && passwordHasUpCase === false && passwordHasLowCase === false && passwordHasNums === false) {
+      alert("You must choose to include one of the aforementioned password criteria to include in your password.");
+      askPassRequirements();
+    }
+
+      // Adds special characters to the generated password array.
+    if (passwordSpecialChar){
+      generatedPass = generatedPass.concat(specChar); 
+    } 
+
+    if (passwordHasUpCase){
+      generatedPass = generatedPass.concat(upperCaseAlpha);
+    } 
+
+    if (passwordHasLowCase){
+      generatedPass = generatedPass.concat(lowerCaseAlpha);
+    } 
+
+    if (passwordHasNums){
+      generatedPass = generatedPass.concat(num);
+    } 
+  }
+
+  askPassRequirements();
+
+  let pluckedPassword = [];
+
+  pluckedPassword.length = passwordLength;
+
+  console.log(pluckedPassword);
+
+  for (var i = 0; i < passwordLength; i++) {
+    pluckedPassword[i] = generatedPass[Math.floor(Math.random() * generatedPass.length)];
+  }
+
+  
+
+  password = pluckedPassword.join("").toString();
+
+  return password;
+
+  // console.log(generatedPass);
+
+  // if (passwordLength > 7 && passwordLength < 129) {
+  //     passwordLength = prompt("Set a password length between 8 and 128 characers.");
+  // }
 }
