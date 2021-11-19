86c520bf40660ec5378e3384ca55ace9a1cf1936
Michael Iringan <miringan@uw.edu>
2021-11-18 18:57:25 -0800

    Cleaned up comments and style on the javascript file. Added content to the README document.

--- script.js
+++ script.js
@@ -13,16 +13,6 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Prompt user for criteria
-// User selects criteria
-// Prompt user for length of password - needs to be between 8 and 128 characters
-// User enters in a password length between 8 and 128 characters
-// Prompt user for character types to include in the password
-// User confirms whether or not to include lowercase, uppercase, numeric, and/or special characters
-// Each answer should be validated and a random selection of the criteria should be selected
-// After all the prompts are answered, a password is generated.
-// The password is then displayed in an alert or written to the page.
-
 // Create and populate array of lower case letters.
 const lowerCaseAlpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
@@ -42,94 +32,81 @@ const specChar = [" ","!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/
 
 
 function generatePassword () {
-  // Prompt for password length.
+  // Define password length.
   var passwordLength;
   
+  // Define function to prompt user for a password length.
   function askPassLength() {
     passwordLength = prompt("Set a password length between 8 and 128 characters.");
 
+    // If statement to prevent password lengths not within range.
     if (passwordLength < 8 || passwordLength > 128) {
       alert("Password length is not within the acceptable length range.");
       askPassLength();
     } 
   } 
 
+  // Call askPassLength function to prompt user for a password length.
   askPassLength();
 
-  // if (passwordLength < 8 || passwordLength > 128) {
-  //   alert("Password length is not within the acceptable length range.");
-  //   passwordLength = prompt("Set a password length between 8 and 128 characers.");
-  // } 
-
-
-
-  // var passwordSpecialChar = confirm("Would you like to include special characers?");
-  // var passwordHasUpCase = confirm("Would you like to include upper case letters?");
-  // var passwordHasLowCase = confirm("Would you like to include lower case letters?");
-  // var passwordHasNums = confirm("Would you like to include numbers?");
-
-  // if (passwordSpecialChar === false && passwordHasUpCase === false && passwordHasLowCase === false && passwordHasNums === false) {
-  //   alert("You must choose to include one of the aforementioned password criteria to include in your password.");
-  //   passwordSpecialChar = confirm("Would you like to include special characers?");
-  //   passwordHasUpCase = confirm("Would you like to include upper case letters?");
-  //   passwordHasLowCase = confirm("Would you like to include lower case letters?");
-  //   passwordHasNums = confirm("Would you like to include numbers?");
-  // }
-
+  // Create array to hold data for a generated password.
   let generatedPass = [];
 
-  let password = "hellow world.";
+  // Create password variable will later be returned.
+  let password;
 
+  // Define function to ask the user what password criteria to use.
   function askPassRequirements () {
     var passwordSpecialChar = confirm("Would you like to include special characers?");
     var passwordHasUpCase = confirm("Would you like to include upper case letters?");
     var passwordHasLowCase = confirm("Would you like to include lower case letters?");
     var passwordHasNums = confirm("Would you like to include numbers?");
 
+    // If statement to make sure the user selects at least one password criteria.
     if (passwordSpecialChar === false && passwordHasUpCase === false && passwordHasLowCase === false && passwordHasNums === false) {
       alert("You must choose to include one of the aforementioned password criteria to include in your password.");
       askPassRequirements();
     }
 
-      // Adds special characters to the generated password array.
+    // Adds special characters to the generated password array.
     if (passwordSpecialChar){
       generatedPass = generatedPass.concat(specChar); 
     } 
 
+    // Adds uppercase letters to the generated password array.
     if (passwordHasUpCase){
       generatedPass = generatedPass.concat(upperCaseAlpha);
     } 
 
+    // Adds lowercase letters to the generated password array.
     if (passwordHasLowCase){
       generatedPass = generatedPass.concat(lowerCaseAlpha);
     } 
 
+    // Adds numbers to the generated password array.
     if (passwordHasNums){
       generatedPass = generatedPass.concat(num);
     } 
   }
 
+  // Call the askPassRequirements function to ask the user what password criteria to use.
   askPassRequirements();
 
+  // Create array to pluck characters from the generatedPass array to set content for new password.
   let pluckedPassword = [];
 
+  // Establish length of pluckedPassword array.
   pluckedPassword.length = passwordLength;
 
-  console.log(pluckedPassword);
-
+  // Populates the pluckedPassword array with randomly selected elements of the generatedPass array.
   for (var i = 0; i < passwordLength; i++) {
     pluckedPassword[i] = generatedPass[Math.floor(Math.random() * generatedPass.length)];
   }
 
-  
-
+  // Set password equal to the string content of the pluckedPassword array.
   password = pluckedPassword.join("").toString();
 
+  // Return password.
   return password;
 
-  // console.log(generatedPass);
-
-  // if (passwordLength > 7 && passwordLength < 129) {
-  //     passwordLength = prompt("Set a password length between 8 and 128 characers.");
-  // }
 }
